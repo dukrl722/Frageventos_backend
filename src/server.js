c5001ebfc4d5c3f2eeb5f2b8e3b1d451574acb19
Eduardo da Silva <dukrl222@hotmail.com>
2022-04-03 17:42:43 -0300

    Criando estrutura dos arquivos

---
+++ src/server.js
@@ -0,0 +1,9 @@
+import express, {Request, Response, NextFunction} from 'express';
+
+const app = express();
+
+app.use(express.json());
+
+app.listen(3333, () => {
+    console.log('🎁, então é natal krl');
+})
